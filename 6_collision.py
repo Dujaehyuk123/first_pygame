ea9ba1f19e5c72607db93e6c942efb92f8d127f9
Dujaehyuk123 <hyouk070812@gmail.com>
2020-10-08 21:56:25 +0900

    done

--- 6_collision.py
+++ 6_collision.py
@@ -27,6 +27,16 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height -  character_height
 
+#ememy character
+
+enemy = pygame.image.load("C:/Users/Hyuk/Desktop/apartment/apartment/apt_price/pygames/enemy.png")
+enemy_size = enemy.get_rect().size
+enemy_width = enemy_size[0]
+enemy_height = enemy_size[1]
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2)
+enemy_y_pos = (screen_height / 2) - (enemy_height / 2)
+
+
 #coordination to move
 to_x = 0
 to_y = 0
@@ -35,10 +45,12 @@ to_y = 0
 character_speed = 0.6
 
 
+
+
 #event loop
 running = True  #is the game proceeding?
 while running:
-    dt = clock.tick(10) #game screen set of fps(frame per second)
+    dt = clock.tick(60) #game screen set of fps(frame per second)
 
     print("fps : " + str(clock.get_fps()))
     for event in pygame.event.get(): #what event occureed?
@@ -74,11 +86,24 @@ while running:
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+    # update rect info for collision process
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
 
-            
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # check collision
+    if character_rect.colliderect(enemy_rect):
+        print("Collided")
+        running = False
+
     screen.blit(background, (0, 0)) #draw background
-   
     screen.blit(character, (character_x_pos, character_y_pos)) #draw character
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) #draw enemy
+
     
     pygame.display.update()
     
